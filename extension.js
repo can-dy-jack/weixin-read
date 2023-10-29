4a62b03f4edc7fcb6f3f13dcf8822161f453947d
can-dy-jack <jjack_chen3@163.com>
2023-10-29 10:41:39 +0800

    实现vscode打开浏览器页面

--- extension.js
+++ extension.js
@@ -1,27 +1,87 @@
 const vscode = require('vscode');
+const path = require('path');
 
-const EventEmitter = require("./lib/event");
+class StatusBar {
+  constructor() {
+    this._statusBar = vscode.window.createStatusBarItem(
+      vscode.StatusBarAlignment.Right,
+      120 // 优先级
+    );
+
+    this._statusBar.text = `$(preview) 微信读书`;
+    this._statusBar.command = 'weixin-read.open';
+    this._statusBar.tooltip = "点击打开微信读书";
+
+    this._statusBar.show();
+  }
+
+  show() {
+    this._statusBar.show();
+  }
+
+  dispose() {
+    this._statusBar.dispose();
+  }
+}
+function createPanel(context) {
+	let panel = vscode.window.createWebviewPanel(
+		'weixin-read',
+		'微信读书',
+		vscode.ViewColumn.One,
+		{
+			enableScripts: true,
+			retainContextWhenHidden: true
+		}
+	);
+
+	panel.iconPath = vscode.Uri.file(
+		path.join(context.extensionPath, 'assets', 'weixin-read.png')
+	);
+
+	panel.webview.html = `<!DOCTYPE html>
+	<html lang="zh">
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>微信读书</title>
+			<style>
+			html,body,iframe{
+				width: 100%;
+				height:100%;
+				border:0;
+				padding:0;
+				overflow: hidden;
+				opacity: 1;
+			}
+			</style>
+		</head>
+		<body>
+			<iframe src="https://weread.qq.com/" id="weixin-read-iframe" />
+		</body>
+	</html>`;
+
+	return panel;
+}
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+	const status = new StatusBar();
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "weixin-read" is now active!');
+	let panel;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('weixin-read.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from weixin-read!');
+	let page = vscode.commands.registerCommand('weixin-read.open', function () {
+		if (!panel) {
+			try {
+				panel = createPanel(context);	
+			} catch (e) {
+				vscode.window.showErrorMessage(e);
+			}
+		}
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(status, page);
 }
 
 function deactivate() {}
